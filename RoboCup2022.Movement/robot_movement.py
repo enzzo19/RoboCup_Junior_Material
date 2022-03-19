f3631cdef20f37fe832afabdf618174ec63a9133
Juarez Enzo <49879701+enzzo19@users.noreply.github.com>
2022-03-19 19:57:00 -0300

    add changes

--- RoboCup2022.Movement/robot_movement.py
+++ RoboCup2022.Movement/robot_movement.py
@@ -2,14 +2,17 @@ from tracemalloc import start
 from controller import Robot
 from controller import Motor
 from controller import PositionSensor
-from controller import Robot, DistanceSensor, GPS, Camera, Receiver, Emitter
+from controller import Robot, DistanceSensor, GPS, Camera, Receiver, Emitter, Gyro
 import math
 import time
+import numpy
 
 robot = Robot() 
 timeStep = 32
 tile_size = 0.12
 speed = 6.28
+angulo_actual = 0
+tiempo_anterior = 0
 
 start = robot.getTime()
 
@@ -17,11 +20,14 @@ start = robot.getTime()
 distancia_sensor1 = robot.getDevice("distance sensor1")
 distancia_sensor1.enable(timeStep)
 
+
 # Motor initialization
 ruedaIzquierda = robot.getDevice("wheel1 motor")
 ruedaDerecha = robot.getDevice("wheel2 motor")
 ruedaIzquierda.setPosition(float('inf'))
 ruedaDerecha.setPosition(float('inf'))
+
+gyro = robot.getDevice("gyro")
 
 # Functions
 def avanzar(vel):
@@ -35,21 +41,41 @@ def girar(vel):
 def girar_izq(vel):
     ruedaIzquierda.setVelocity(vel)
     ruedaDerecha.setVelocity(-vel)
+    
+    
+def rotar(angulo):
+    global angulo_actual
+    global tiempo_anterior
+    #  iniciar_rotacion
+    girar(0.5)
+    # Mientras no llego al angulo solicitado sigo girando
+    if (abs(angulo - angulo_actual) > 1):
+        tiempo_actual = robot.getTime()
+        # print("Inicio rotacion angulo", angulo, "Angulo actual:",angulo_actual)
+        tiempo_transcurrido = tiempo_actual - \
+            tiempo_anterior  # tiempo que paso en cada timestep
+        # rad/seg * mseg * 1000
+        radsIntimestep = abs(gyro.getValues()[1]) * tiempo_transcurrido
+        degsIntimestep = radsIntimestep * 180 / math.pi
+        # print("rads: " + str(radsIntimestep) + " | degs: " + str(degsIntimestep))
+        angulo_actual += degsIntimestep
+        # Si se pasa de 360 grados se ajusta la rotacion empezando desde 0 grados
+        angulo_actual = angulo_actual % 360
+        # Si es mas bajo que 0 grados, le resta ese valor a 360
+        if angulo_actual < 0:
+            angulo_actual += 360
+        tiempo_anterior = tiempo_actual
+        return False
+    print("Rotacion finalizada.")
+    angulo_actual = 0
+    return True
+
+
+gyro.enable(timeStep)
+print("Valor del Giroscopo \n: ")
+print(gyro.getValues())
 
 while robot.step(timeStep) != -1:
     avanzar(6.28)
-    for i in range(2):
-        if robot.getTime() >= start + 4.8:
-            girar(6.28)
-            if robot.getTime() >= start + 5.15:
-                avanzar(6.28)
-                if robot.getTime() >= start + 9.91:
-                    girar(6.28)
-                    if robot.getTime() >= start + 10.26:
-                        avanzar(6.28)
-                        if robot.getTime() >= start + 15.2:
-                            girar(6.28)
-                            if robot.getTime() >= start + 16:
-                                avanzar(6.28)
-                                if robot.getTime() >= start + 16.3:
-                                    avanzar(0)
+    if distancia_sensor1.getValues() < 0.06:
+        rotar(90)
